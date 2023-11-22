0653ba955ce7d0a68ea2e90e2280b87febbcf228
xxxlphz <144443584+xxxlphz@users.noreply.github.com>
2023-11-22 13:16:37 +0000

    Add files via upload

---
+++ reading numbers.py
@@ -0,0 +1,22 @@
+# file = open("C:\\Users\\19AAbdallah.ACC\\thonny\\shelley.txt",'r')
+# 
+# for line in file:
+#     
+#     print(line.strip())
+# 
+# 
+# 
+# file.close()
+
+
+
+# file = open("C:\\Users\\19AAbdallah.ACC\\thonny\\numbers.txt",'r')
+# tot = 0
+# for line in file:
+#     x =line.strip()
+#     if x !=
+# 
+# 
+# file.close()
+
+
