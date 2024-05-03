5b60bb36543f2356e7cc895396fdbbdb183d54cc
xxxlphz <144443584+xxxlphz@users.noreply.github.com>
2024-05-03 12:35:55 +0100

    Update aaron's mastermind WITH FEATURES.py

--- algorithims/flowcharts/aaron's mastermind WITH FEATURES.py
+++ algorithims/flowcharts/aaron's mastermind WITH FEATURES.py
@@ -3,13 +3,17 @@ from colorama import Fore, Back, Style
 import random
 guesses = 0
 
-green = Fore.GREEN + "green"
-red = Fore.RED + "red"
-blue = Fore.BLUE + "blue"
-yellow = Fore.YELLOW + "yellow"
-norm = Fore.WHITE
-possiblecolors = [red,green,blue,yellow]
+dim = Fore.WHITE + Style.NORMAL
+green = Fore.GREEN + "(g)reen"
+red = Fore.RED + "(r)ed"
+blue = Fore.BLUE + "(b)lue"
+yellow = Fore.YELLOW + "(y)ellow"
+purp = Fore.MAGENTA + Style.BRIGHT + "(p)urple"
+norm = Fore.WHITE + Style.BRIGHT
+possiblecolors = [red,green,blue,yellow,purp]
 colors = []
+
+print(norm)
 
 for x in range(4):
     z = random.randint(0,3)
@@ -27,30 +31,48 @@ RULES:
 '''
 print(rules)
 print('POSSIBLE COLOURS: ',end='');print(*possiblecolors, norm)
+print('*For rules, enter \'rules\'')
+print(' For list of colours, enter \'colours\'\n', norm)
 while True:
     guess = []
     for x in range(4):
         mycolour = input(f'enter colour {x+1}: ')
-        if mycolour == 'red':
-            guess.append(red)
-        elif mycolour == 'green':
-            guess.append(green)
-        elif mycolour == 'blue':
-            guess.append(blue)
-        elif mycolour == 'yellow':
-            guess.append(yellow)
+        while 1:
+            if mycolour.lower() == 'rules':
+                print(rules)
+                break
+            elif mycolour.lower() == 'colours' or mycolour.lower() == 'colors':
+                print(*possiblecolors,norm)
+                break
+            elif mycolour.lower() == 'red' or mycolour.lower() == 'r':
+                guess.append(red)
+                break
+            elif mycolour.lower() == 'green' or mycolour.lower() == 'g':
+                guess.append(green)
+                break
+            elif mycolour.lower() == 'blue' or mycolour.lower() == 'b':
+                guess.append(blue)
+                break
+            elif mycolour.lower() == 'purple' or mycolour.lower() == 'p':
+                guess.append(purp)
+                break
+            elif mycolour.lower() == 'yellow' or mycolour.lower() == 'y':
+                guess.append(yellow)
+                break
+            else:
+                mycolour = input(f'!!! INVALID: ')
     returned = []
     guesses += 1
     for i in range(4):
         if guess[i] == colors[i]:
-            returned.append('correct')
+            returned.append(f'{dim}correct')
         else:
             if guess[i] in colors:
-                returned.append('partial')
+                returned.append(f'{dim}partial')
             else:
-                returned.append('incorrect')
+                returned.append(f'{dim}incorrect')
     print('your guess:',*guess,norm)
-    print(returned,end=('\n\n'))
+    print(*returned,end=('\n\n'))
     
     if guess == colors:
         break
