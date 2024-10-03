fe0e753fd052760cb994be279a55ee46cb741966
xxxlphz <144443584+xxxlphz@users.noreply.github.com>
2024-10-03 15:22:49 +0100

    Create pandas intro (incomplete).py

---
+++ pandas intro (incomplete).py
@@ -0,0 +1,18 @@
+import pandas as pd
+df = pd.read_csv('unclean_data 1.csv',encoding = 'ANSI') # opens csv file same way youd read it normally
+ 
+columns = df.columns # prints name of colums (keys of the table dictionary)
+columns = str(df.columns).upper() #makes it upper, df.columns.str.upper() works too
+print(columns)
+ 
+#to rename
+#df = df.rename(columns={'oldName1': 'newName1', 'oldName2': 'newName2'}) 
+df = df.rename(columns = {'DURATION' : 'LENGTH'})
+print(columns)
+
+#missing data
+pd.set_option('display.max_columns',None)
+print(df.isnull(),'\n')# shows if data is missing from a data point, true = empty, false = theres data
+print(df.isnull().any(),'\n') # shows each column instead of each spot
+print(df.isnull().sum(),'\n') # shows how many empty spots in each column
+print(df.isnull().sum().sum(),'\n') # shows how many empty spots in whole file
